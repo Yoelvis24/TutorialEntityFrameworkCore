0eff82e7f891344078b534a5a414ce7e41132432
Yoelvis Perez <yoelvis.perez.20017@gmail.com>
2021-10-02 22:17:24 -0400

    Creando migración "AgregandoKey" para incrementar el Id

---
+++ TutorialEntityFrameworkCore/Migrations/20211003020656_AgregandoKey.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutorialEntityFrameworkCore.Migrations
+{
+    public partial class AgregandoKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- TutorialEntityFrameworkCore/Models/Student.cs
+++ TutorialEntityFrameworkCore/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,6 +14,7 @@ namespace TutorialEntityFrameworkCore.Models
             StudentCourses = new HashSet<StudentCourse>();
         }
 
+        [Key]
         public int StudentId { get; set; }
         public int? GradeId { get; set; }
         public string FirstName { get; set; }
